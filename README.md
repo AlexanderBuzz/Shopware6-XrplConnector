3b665eb41b560082a7ab4c335ca6410f7c15a6e9
AlexanderBuzz <alexander.busse@posteo.net>
2022-09-19 18:07:10 +0200

    Initial Commit / Skeleton

---
+++ README.md
@@ -0,0 +1,7 @@
+# XrplConnector Plugin [WIP]
+
+Plugin to make XRP payments in Shopware 6
+
+## Prerequisites - TBD
+
+## Installation - TDB
---
+++ src/XrplConnector.php
@@ -0,0 +1,9 @@
+<?php declare(strict_types=1);
+
+namespace XrplConnector;
+
+use Shopware\Core\Framework\Plugin;
+
+class XrplConnector extends Plugin
+{
+}
